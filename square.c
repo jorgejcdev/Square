27c365b9c68628f899afd481b362b43e08f3e82c
jorgejcdev <110706936+jorgejcdev@users.noreply.github.com>
2022-11-02 22:19:22 -0300

    Add files via upload

---
+++ square.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+    int i,j;
+
+    for(i = 1; i<= 6 ; i++){
+       printf(" @");
+     }
+
+       printf("\n");
+ 
+    for(i = 2;i <6;i++) {
+       printf(" @");
+   
+    for(j = 2;j < 6;j++)   {
+       printf("  ");           
+   }
+       printf(" @  \n");
+
+ }
+
+     for(i = 1; i<= 6 ; i++){       
+       printf(" @");
+
+ }
+}
